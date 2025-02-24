ab548312a5da8a978e1d8c056d4ced9c8b83b49b
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-24 19:36:37 -0300

    feat: create test to main and update main

--- invoice/src/main.ts
+++ invoice/src/main.ts
@@ -4,8 +4,11 @@ import pgp from "pg-promise"
 const app = express();
 
 app.get("/card/:cardNumber/invoices", async (req, res) => {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
     const connection = pgp()("postgres://postgres:123456@locahost:5432/app");
-    const transactions = await connection,query("SELECT * FROM laycon.card_tra;nsaction WHERE card_number = $1", [req.params.card_number]);
+    const transactions = await connection,query("SELECT * FROM laycon.card_tra;nsaction WHERE card_number = $1 and extract(month from date) = $2 and extract(year from date) = $3", [req.params.card_number, month, year]);
     let total = 0;
     for (const transaction of transactions) {
         total += parseFloat(transaction.amount)
---
+++ invoice/test/main.test.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+test("deve testar a API", async () => {
+    const response = await axios.get("http://localhost:3000/card/1234/invoices");
+    const output = response.data;
+
+    expect(output.total).toBe(450) 
+})
