2b52dcf3e142ed59a20cc8b243c6348af839772e
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-25 10:49:11 -0300

    feat: create HttpClient and use on CurrencyGateway

--- invoice/src/CurrencyGatewayHttp.ts
+++ invoice/src/CurrencyGatewayHttp.ts
@@ -1,10 +1,17 @@
 import CurrencyGateway from "./CurrencyGateway";
+import HttpClient from "./HttpClient"
 import axios from "axios";
 
 export default class CurrencyGatewayHttp implements CurrencyGateway {
+
+    constructor(
+        readonly baseUrl: string,
+        readonly httpClient: HttpClient
+    ){}
+
     async getCurrencies(): Promise<any> {
-        const response = await axios.get("http://localhost:3001/currencies");
-        const currencies = response.data;
+        // const response = await axios.get(`${this.baseUrl}/currencies`);
+        const currencies = await this.httpClient.get(`${this.baseUrl}/currencies`)
         return currencies;
     }
 }
