bc295747bbecf452c4cdf68d162151d6deff510b
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-25 10:54:57 -0300

    fix: use axios adapter as httpclient on main

--- invoice/src/main.ts
+++ invoice/src/main.ts
@@ -2,13 +2,16 @@ import express from "express";
 import CalculateInvoice from "./CalculateInvoice";
 import TransactionDAODatabase from "./TransactionDAODatabase";
 import CurrencyGatewayHttp from "./CurrencyGatewayHttp";
+import AxiosAdapter from "./AxiosAdaper";
 
 
 const app = express();
 
 app.get("/card/:cardNumber/invoices", async (req, res) => {
     const transactionDAO = new TransactionDAODatabase();
-    const currencyGateway = new CurrencyGatewayHttp();
+    const httpClient = new AxiosAdapter();
+    const baseUrl = "http://localhost:3001"
+    const currencyGateway = new CurrencyGatewayHttp(baseUrl, httpClient);
     const calculateInvoice = new CalculateInvoice(transactionDAO, currencyGateway);
     const total = await calculateInvoice.execute(req.params.cardNumber);
     res.json({
