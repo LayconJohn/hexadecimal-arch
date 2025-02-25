a01da8b4f5bca7cf8aba668601c58affbe7e66d9
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-25 11:06:00 -0300

    feat: create PgPromiseAdapter

---
+++ invoice/src/PgPromiseAdapter.ts
@@ -0,0 +1,17 @@
+import Connection from "./Connection";
+import pgp from "pg-promise"
+
+export default class PgPromiseAdapter implements Connection {
+    connection: any; 
+
+    constructor(){
+        this.connection = pgp()("postgres://postgres:123456@locahost:5432/app");
+    }
+
+    async query(statement: string, params: any): Promise<any> {
+        return this.connection.query(statement, params);
+    }
+    async close(): Promise<any> {
+        this.connection.$pool.end()
+    }
+}
