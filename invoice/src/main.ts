a7c9aea8d0f67feb6979ce33e3161244126b2b93
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-25 09:29:49 -0300

    feat: create CalculateInvoice and Calculate incoice test

--- invoice/src/main.ts
+++ invoice/src/main.ts
@@ -1,24 +1,12 @@
 import express from "express";
-import pgp from "pg-promise"
-import axios from "axios";
+import CalculateInvoice from "./CalculateInvoice";
+
 
 const app = express();
 
 app.get("/card/:cardNumber/invoices", async (req, res) => {
-    const today = new Date();
-    const month = today.getMonth() + 1;
-    const year = today.getFullYear();
-    const response = await axios.get("http://localhost:3001/currencies");
-    const currencies = response.data;
-
-    const connection = pgp()("postgres://postgres:123456@locahost:5432/app");
-    const transactions = await connection.query("SELECT * FROM laycon.card_tra;nsaction WHERE card_number = $1 and extract(month from date) = $2 and extract(year from date) = $3", [req.params.card_number, month, year]);
-    let total = 0;
-    for (const transaction of transactions) {
-        if(transaction.currency === "BRL") total += parseFloat(transaction.amount);
-        if(transaction.currency === "USD") total += parseFloat(transaction.amount) * currencies.usd;
-        
-    }
+    const calculateInvoice = new CalculateInvoice();
+    const total = await calculateInvoice.execute(req.params.cardNumber);
     res.json({
         total
     })
