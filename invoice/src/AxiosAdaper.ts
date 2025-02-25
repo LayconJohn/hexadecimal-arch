e3c5682de43985cfe08eb1690da8b84e62332253
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-25 10:52:48 -0300

    feat: create AxiosAdapter

---
+++ invoice/src/AxiosAdaper.ts
@@ -0,0 +1,13 @@
+import HttpClient from "./HttpClient";
+import axios from "axios";
+
+export default class AxiosAdapter implements HttpClient {
+    async get(url: string): Promise<any> {
+        const response = axios.get(url)
+        return response.data; 
+    }
+    async post(url: string, body: any): Promise<any> {
+        const response = axios.post(url, body)
+        return response.data; 
+    }
+}
