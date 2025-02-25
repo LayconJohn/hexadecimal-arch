9fd8390ff77edd18d01a09111ff2013e04480dea
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-25 09:54:51 -0300

    test: update CalculateInvoice test

--- invoice/test/CalculateInvoice.test.ts
+++ invoice/test/CalculateInvoice.test.ts
@@ -1,7 +1,25 @@
 import CalculateInvoice from "../src/CalculateInvoice";
+import CurrencyGateway from "../src/CurrencyGateway";
+import TransactionDAO from "../src/TransactionDAO";
 
 test("Deve calcular a fatura", async () => {
-    const calculateInvoice = new CalculateInvoice();
+    const transactionDAO: TransactionDAO = {
+        async getTransactions(cardNumber, month, year): Promise<any> {
+            return [
+                { currency: "BRL", amount: 100 },
+                { currency: "BRL", amount: 1000 },
+                { currency: "USD", amount: 600 },
+            ];
+        }
+    };
+    const currencyGateway: CurrencyGateway = {
+        async getCurrencies(): Promise<any> {
+            return {
+                usd: 2
+            }
+        }
+    }
+    const calculateInvoice = new CalculateInvoice(transactionDAO, currencyGateway);
     const total = await calculateInvoice.execute("1234");
-    expect(total).toBe(1050);
+    expect(total).toBe(2300);
 })
