3190431eeaea00282adf9e13931bc077dc4202e9
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-25 11:00:42 -0300

    feat: create interface Connection

--- invoice/src/TransactionDAODatabase.ts
+++ invoice/src/TransactionDAODatabase.ts
@@ -1,10 +1,15 @@
+import Connection from "./Connection";
 import TransactionDAO from "./TransactionDAO";
-import pgp from "pg-promise"
 
 export default class TransactionDAODatabase implements TransactionDAO{
+
+    constructor(
+        readonly connection: Connection
+    ) {}
+
     async getTransactions(cardNumber: string, month: number, year: number): Promise<any> {
-        const connection = pgp()("postgres://postgres:123456@locahost:5432/app");
-        const transactions = await connection.query("SELECT * FROM laycon.card_tra;nsaction WHERE card_number = $1 and extract(month from date) = $2 and extract(year from date) = $3", [cardNumber, month, year]);
+        // const connection = pgp()("postgres://postgres:123456@locahost:5432/app");
+        const transactions = await this.connection.query("SELECT * FROM laycon.card_transaction WHERE card_number = $1 and extract(month from date) = $2 and extract(year from date) = $3", [cardNumber, month, year]);
         return transactions;
     }
 
