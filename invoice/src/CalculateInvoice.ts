dd95f0108421f3f0106c3aca8192df96d0f6c880
Laycon Gomes <laycon.gomes@tld.com.br>
2025-02-25 10:02:08 -0300

    feat: create TransactionDAODatabase and CurencyGatewayHttp

--- invoice/src/CalculateInvoice.ts
+++ invoice/src/CalculateInvoice.ts
@@ -1,5 +1,4 @@
-import pgp from "pg-promise"
-import axios from "axios";
+
 import TransactionDAO from "./TransactionDAO";
 import CurrencyGateway from "./CurrencyGateway";
 
@@ -14,12 +13,9 @@ export default class CalculateInvoice {
         const month = today.getMonth() + 1;
         const year = today.getFullYear();
         const currencies = await this.currencyGateway.getCurrencies();
-        // const response = await axios.get("http://localhost:3001/currencies");
-        // const currencies = response.data;
 
         const transactions = await this.transactionDAO.getTransactions(cardNumber, month, year);
-        // const connection = pgp()("postgres://postgres:123456@locahost:5432/app");
-        // const transactions = await connection.query("SELECT * FROM laycon.card_tra;nsaction WHERE card_number = $1 and extract(month from date) = $2 and extract(year from date) = $3", [cardNumber, month, year]);
+
         let total = 0;
         for (const transaction of transactions) {
             if(transaction.currency === "BRL") total += parseFloat(transaction.amount);
